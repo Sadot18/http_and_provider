1ee6843fe12bfc2a6e02d4133e8a13ee55fca64b
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-21 20:27:49 -0600

    agregar estructura básica de la aplicación HomeApp

--- lib/home_app.dart
+++ lib/home_app.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class HomeApp extends StatelessWidget {
   const HomeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('Card Swiper')),
-      body: Center(child: Text('Hola mundo')),
+      appBar: AppBar(centerTitle: true, title: Text('....')),
+      body: Column(
+        children: [
+          Swiper(
+            itemCount: 10,
+            autoplay: true,
+            itemHeight: size.height * 0.5,
+            itemWidth: 200,
+            layout: SwiperLayout.STACK,
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  'https://picsum.photos/200/300?random=$index',
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
