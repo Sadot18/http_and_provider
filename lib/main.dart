22cfa67dd0b201b02dc2faa1640b404b96d6fbf0
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-21 21:45:39 -0600

    refactor: implementar MyProvider para gestionar el estado con MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:http_and_provider/home_app.dart';
+import 'package:http_and_provider/providers/movie_provider.dart';
+import 'package:provider/provider.dart';
+
 void main() {
-  runApp(MainApp());
+  runApp(MyProvider());
+}
+
+class MyProvider extends StatelessWidget {
+  const MyProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Inyectando
+    return MultiProvider(
+      providers: [
+        //TODO: Instanciado                                TODO: Carga perezosa
+        ChangeNotifierProvider(create: (_) => MovieProvider(), lazy: false),
+      ],
+      child: MainApp(), // lazy Load
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
